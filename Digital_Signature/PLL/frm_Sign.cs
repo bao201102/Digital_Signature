9d84d4c4a565aac4d8f5c8c08707db3533de12f6
Đoàn Trần Bá Đạt <89367921+doandat8888@users.noreply.github.com>
2022-10-03 13:32:06 +0700

    sdsf
    sdf

--- Digital_Signature/PLL/frm_Sign.cs
+++ Digital_Signature/PLL/frm_Sign.cs
@@ -29,7 +29,32 @@ namespace Digital_Signature
         {
             if (true)
             {
-                bunifuSnackbar1.Show(this, "Bạn đã ký văn bản thành công", Bunifu.UI.WinForms.BunifuSnackbar.MessageTypes.Success);
+                string fullName = txtName.Text;
+                string gender = cbGender.Text;
+                string graYear = txtYear.Text;
+                string email = txtEmail.Text;
+                string birthPlace = txtBorn.Text;
+                string phone = txtPhone.Text;
+                string ethic = cbReligion.Text;
+
+                string[] infoArr = { fullName, gender, graYear, email, birthPlace, phone, ethic };
+                int count = 0;
+                for(int i = 0; i < infoArr.Length; i++)
+                {
+                    if (infoArr[i] != "")
+                    {
+                        count++;
+                    }
+                }
+                if(count == infoArr.Length)
+                {
+
+                }else
+                {
+                    bunifuSnackbar1.Show(this, "Vui lòng nhập đủ thông tin", Bunifu.UI.WinForms.BunifuSnackbar.MessageTypes.Warning);
+                }
+
+                //bunifuSnackbar1.Show(this, "Bạn đã ký văn bản thành công", Bunifu.UI.WinForms.BunifuSnackbar.MessageTypes.Success);
             }
         }
 
