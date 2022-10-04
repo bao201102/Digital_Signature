c1ea96fb55988ee3ff09a1d9e8e5a7b79f5902d9
Lồ Sìn Dậu <luoshanyou75588@gmail.com>
2022-10-04 13:10:45 +0700

    update database

--- Digital_Signature/Program.cs
+++ Digital_Signature/Program.cs
@@ -16,7 +16,7 @@ namespace Digital_Signature
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frm_Sign());
+            Application.Run(new frm_Login());
         }
     }
 }
